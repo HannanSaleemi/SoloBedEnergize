2562e19cca8bf80759606089365fb1b475f6219a
Hannan Saleemi <hannansaleemi1@gmail.com>
2020-11-04 15:29:14 +0000

    Setup Structure and added Bed Detection and Stamina  increase

---
+++ SoloBedEnergize/ModEntry.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace SoloBedEnergize
+{
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.GameLoop.OneSecondUpdateTicking += this.updateStaminaForFarmer;
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        private void updateStaminaForFarmer(object sender, OneSecondUpdateTickingEventArgs e)
+        {
+            if (!Context.IsWorldReady)
+                return;
+
+            if (Game1.serverHost != null)
+                return;
+
+            if (Game1.player.isInBed)
+                Game1.player.Stamina += (float)(2);
+            
+        }
+    }
+}
